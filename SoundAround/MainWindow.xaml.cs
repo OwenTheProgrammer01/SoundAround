3b4439ec7d4219eb7010614cda2a47aae28af4dc
Owen Bal <owen.bal@leerling.guldensporencollege.be>
2022-05-23 23:43:13 +0200

    .

--- SoundAround/MainWindow.xaml.cs
+++ SoundAround/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
 
 namespace SoundAround
 {
@@ -20,9 +22,55 @@ namespace SoundAround
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Song> Songs = new List<Song>();
+        List<Bestandtype> Bestandtypen = new List<Bestandtype>();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public void DatabaseOphalen()
+        {
+            Bestandtypen = BestandtypeDA.Ophalen();
+            Songs = SongDA.Ophalen();
+        }
+
+        private void btnPlayPause_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnUpload_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.DefaultExt = ".wav";
+            file.Filter = "WAV-bestand (.wav)|* .wav";
+
+            if (file.ShowDialog() == true)
+            {
+                Bestandtype bestandtype = new Bestandtype();
+                Artiest artiest = new Artiest();
+                Genre genre = new Genre();
+                Album album = new Album();
+                Song song = new Song();
+                BinaryReader br = new BinaryReader(file.OpenFile());
+
+                bestandtype.bestandtype = file.DefaultExt;
+                BestandtypeDA.Toevoegen(bestandtype);
+                DatabaseOphalen();
+                foreach (Bestandtype _bestandtype in Bestandtypen)
+                {
+                    if (_bestandtype == bestandtype)
+                    {
+                        song.Bestandtype_ID = _bestandtype.Bestandtype_ID;
+                    }
+                }
+                song.Bestand = br.ReadBytes(Convert.ToInt32(song.Duur));
+                song.Naam = file.SafeFileName;
+                song.Duur = TimeSpan.FromSeconds(file.OpenFile().Length);
+
+            }
         }
     }
 }
